801aa9d3ba58a2f24630d4f07c90a1c7f9622fc3
maddieannette <64941576+maddieannette@users.noreply.github.com>
2021-03-25 20:55:17 -0500

    Final loop through multiple pages

--- Data-Scraping/main.py
+++ Data-Scraping/main.py
@@ -1,57 +1,47 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
+from time import sleep
+from random import randint
+import numpy as np
+import pandas as pd
 
 # retrieve data from ulta 
-
-def pageturning():
-    openPage = open("makeup.text", "w+")
-    pageCount = 0 
-
-    while pageCount>= 0:
-        pageCount += 1
-        urlPage = 'https://www.ulta.com/makeup-lips?N=26yq' + '26yq' + str(pageCount) + '.html'
-        refer = urlPage
-    try:
-            req = urllib.request.Request(url = urlPage, headers=headers)
-            response = urlopen(req)
-            html = response.read()
-        except error,HTTPError as e: 
-            break 
-        soup = BeautifulSoup(page.content, 'html.parser')
-        page = soup.find_all(class_="next")
-#counter
-
-def getNextPage(soup):
-    page = soup.find('ul', class_='floatr pagination-select')
-    if not page.find('li', class_='next'):
-        url = 'https://www.ulta.com/' + str(page.find('li', class_='prev')).find('a')['href'])
-        return url
-    else:
-        return
-
-while True:
-    soup = getData(url)
-    url = getNextPage(soup)
-    if not url:
-        break 
-    print(url)
-pages = soup.find_all(class=_'next')
-
-# URL = 'https://www.ulta.com/makeup-lips?N=26yq'
-# page = requests.get(URL)
-# soup = BeautifulSoup(page.content, 'html.parser')
-# product = soup.find_all('div', class_='productQvContainer')
-# for eachProduct in product: 
-#     brand = eachProduct.find('h4', class_='prod-title')
-#     price = eachProduct.find('span', class_='regPrice')
-#     productType = eachProduct.find('p', class_='prod-desc')
-#     if None in (brand, price, productType):
-#         continue
-#     print(brand.text.strip())
-#     print(price.text.strip())
-#     print(productType.text.strip())
-#     print()
-
+firstProduct = 0
+showPerPage = 50
+call = 1
+keepGoing = True
+while call <= 100 and keepGoing:
+    keepGoing = False
+    call += 1
+    URL = 'https://www.ulta.com/makeup-lips?N=26yq&No='+ str(firstProduct) +'&Nrpp=' + str(showPerPage)
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    products = soup.find_all('div', class_='productQvContainer')
+    sleep(randint(2, 10))
+    print(page)
+    for eachProduct in products: 
+        keepGoing = True 
+        brand = eachProduct.find('h4', class_='prod-title')
+        price = eachProduct.find('span', class_='regPrice')
+        productType = eachProduct.find('p', class_='prod-desc')
+        if None in (brand, price, productType):
+            continue
+        print(brand.text.strip())
+        print(price.text.strip())
+        print(productType.text.strip())
+        print()
+    firstProduct = firstProduct + showPerPage
+        
+        
+# allProducts = pd.DataFrame(
+# {
+#     "Brand": brand,
+#     "Price": price,
+#     "Name": productType,
+# })
+# print(allProducts)
+# allProducts.to_csv(r"/Users/madelineflores/Documents/allproducts.csv", index="False", header="True" )
 
 
 # after installing/importing beautiful soup allows to take html and parse as needed 
