b22b0eaea152bcd321ea1de04a30a31c3dacbf56
maddieannette <64941576+maddieannette@users.noreply.github.com>
2021-03-30 21:30:38 -0500

    pulled data in csv

--- Data-Scraping/main.py
+++ Data-Scraping/main.py
@@ -6,13 +6,12 @@ from random import randint
 import numpy as np
 import pandas as pd
 
+
+
 # retrieve data from ulta 
 firstProduct = 0
 showPerPage = 50
 call = 1
-file = open('alllipsticks.csv', 'wb')
-writer = csv.writer(file)
-writer.writerow(['Brand', 'Price', 'ProductType'])
 keepGoing = True
 while call <= 100 and keepGoing:
     keepGoing = False
@@ -23,47 +22,19 @@ while call <= 100 and keepGoing:
     products = soup.find_all('div', class_='productQvContainer')
     sleep(randint(2, 10))
     print(page)
-    for eachProduct in products: 
-        keepGoing = True 
-        brand = eachProduct.find('h4', class_='prod-title')
-        price = eachProduct.find('span', class_='regPrice')
-        productType = eachProduct.find('p', class_='prod-desc')
-        if None in (brand, price, productType):
-            continue
-        print(brand.text.strip())
-        print(price.text.strip())
-        print(productType.text.strip())
-        print()
-        writer.writerow([brand.encode('utf-8').strip(), price.encode('utf-8').strip(), PpoductType.encode('utf-8').strip()])
-    firstProduct = firstProduct + showPerPage
-        
-file.close()
-        
-# allProducts = pd.DataFrame(
-# {
-#     "Brand": brand,
-#     "Price": price,
-#     "Name": productType,
-# })
-# print(allProducts)
-# allProducts.to_csv(r"/Users/madelineflores/Documents/allproducts.csv", index="False", header="True" )
-
-
-# after installing/importing beautiful soup allows to take html and parse as needed 
-# soup = BeautifulSoup(page.content, 'html.parser')
-
-
-# # find the container for all the variables 
-
-# product = soup.find_all('div', class_='productQvContainer')
-
-# for eachProduct in product: 
-#     brand = eachProduct.find('h4', class_='prod-title')
-#     price = eachProduct.find('span', class_='regPrice')
-#     productType = eachProduct.find('p', class_='prod-desc')
-#     if None in (brand, price, productType):
-#         continue
-#     print(brand.text.strip())
-#     print(price.text.strip())
-#     print(productType.text.strip())
-#     print()
+    with open("product_data.csv", "a") as csv_file:
+        writer = csv.writer(csv_file)
+        for eachProduct in products: 
+            keepGoing = True 
+            brand = eachProduct.find('h4', class_='prod-title')
+            price = eachProduct.find('span', class_='regPrice')
+            productType = eachProduct.find('p', class_='prod-desc')
+            if None in (brand, price, productType):
+                continue
+            # print(brand.text.strip())
+            # print(price.text.strip())
+            # print(productType.text.strip())
+            # writer.writerow([thisBrand, thisPrice, thisProductType])
+            writer.writerow([brand.text.strip(), price.text.strip(), productType.text.strip()])
+        firstProduct = firstProduct + showPerPage
+    
